8ad9d111ba7b24996d00665a84475a750858ae5f
Jose Pejuan <jose.pejuan@unitec.edu>
2014-09-05 15:42:36 -0600

    Investigadores clase: subida

---
+++ Investigadores.cpp
@@ -0,0 +1,12 @@
+#include "Investigadores.h"
+#include "Persona.h"
+#include <string>
+#include <sstream>
+using namespace std;
+Investigadores::Investigadores(string nombre,string usuario,int edad,string id,string fecha_nac,int casos,int casosCerrados,int casosSinResolver):Persona(nombre,usuario,edad,id,fecha_nac){
+	this->casos=casos;
+	this->casosCerrados=casosCerrados;
+	this->casosSinResolver=casosSinResolver;
+}
+Investigadores::Investigadores(){}
+Investigadores::~Investigadores(){}
---
+++ Investigadores.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include "Persona.h"
+using namespace std;
+class Investigadores{
+	int casos,casosCerrados,casosSinResolver;
+public:
+	Investigadores();
+	Investigadores(string,string,int,string,string,int,int,int);
+	~Investigadores();
+	void setCasos(int);
+	void setCasosCerrados(int);
+	void setCasosSinResolver(int);
+	
+};
