4fc6464a177cae92b5a5b1206a864221620f941b
Jose Pejuan <jose.pejuan@unitec.edu>
2014-09-05 14:33:46 -0600

    Agregando Persona.cpp

---
+++ Persona.cpp
@@ -0,0 +1,30 @@
+#include "Persona.h"
+#include <string>
+#include <stream>
+using namespace std;
+Persona::Persona(){
+
+}
+Persona::Persona(string nombre,string usuario,int edad,string id,string fecha_nac){
+	this->nombre=nombre;
+	this->usuario=usuario;
+	this->edad=edad;
+	this->id=id;
+	this->fecha_nac=fecha_nac;
+}
+string Persona::toString() const;
+void Persona::setNombre(string nombre){
+	this->nombre=nombre;
+}
+void Persona::setUsuario(string usuario){
+	this->usuario=usuario;
+}
+void Persona::setEdad(int edad){
+	this->edad=edad;
+}
+void Persona::setId(string Id){
+	this->id=id;
+}
+void Persona::setFecha_nac(string fecha_nac){
+	this->fecha_nac=fecha_nac;
+}
