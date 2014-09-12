0bdb268e454ec90d251da4ae2068134756fcc947
Jose Pejuan <jose.pejuan@unitec.edu>
2014-09-12 13:46:14 -0600

    Corrigiendo

--- Caso.cpp
+++ Caso.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 Caso::Caso(int numero_caso, vector<Evidencias> evidencia, string hora, string fecha, bool completado){
 	this->numero_caso = numero_caso;
-	this->investigadores = investigadores;
 	this->evidencia = evidencia;
 	this->hora = hora;
 	this->fecha = fecha;
@@ -21,10 +20,6 @@ Caso::Caso(){
 }
 void Caso::setNumeroCaso(int numero_caso){
 	this->numero_caso = numero_caso;
-}
-
-void Caso::setInvestigadores(vector<Investigadores> investigadores){
-	this->investigadores = investigadores;
 }
 
 void Caso::setEvidencia(vector<Evidencias> evidencia){
@@ -45,20 +40,7 @@ void Caso::setCompletado(bool completado){
 
 string Caso::toString() const{
 	stringstream ss;
-	ss.str("");
-	ss<<"Numero de caso: "<<numero_caso<<endl;
-	for (int i = 0; i < investigadores.size(); ++i){
-		ss<<"Investigador "<<(i+1)<<": "<<(investigadores[i]).toString()<<endl;	
-	}
-	for (int i = 0; i < evidencia.size(); ++i){
-		ss<<"Evidencia "<<(i+1)<<": "<<(evidencia[i]).toString()<<endl;
-	}
-	ss<<"Hora: "<<hora<<endl;
-	ss<<"Fecha: "<<fecha<<endl;
-	if (completado == true){
-		ss<<"Completado: si"<<endl;
-	}else {
-		ss<<"Completado: no"<<endl;
-	}
+	ss<<"Hola";
+	
 	return ss.str();
 }
